c5e67a1671e45e05df539969f2d366a4cd58de58
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-03-03 18:23:13 -0600

    skeleton

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Login from './Login.tsx';
+import Home from './Home.tsx';
+import ItemDetails from './ItemDetails.tsx';
+import Cart from './Cart.tsx';
+import { CartProvider } from './CartContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/item/:id" element={<ItemDetails />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   </StrictMode>,
-)
+);
